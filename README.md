7bdfed11372c7bdcfd0fbdfcbeeaaa029035f3d7
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-02-13 18:36:47 -0600

    Write file function.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ReadME
+  ## Table of Contents
+  [Description](https://github.com/CaitlinCollins/ReadME#description)
+  [Installation](https://github.com/CaitlinCollins/ReadME#installation)
+  [Usage](https://github.com/CaitlinCollins/ReadME#usage)
+  [Contributing](https://github.com/CaitlinCollins/ReadME#contributing)
+  [Tests](https://github.com/CaitlinCollins/ReadME#tests)
+  [License](https://github.com/CaitlinCollins/ReadME#license)
+  [Contact](https://github.com/CaitlinCollins/ReadME#contact)
+  ## Description
+  This is a ReadME generator
+  ## Installation
+  install npm inquirer
+  ## Usage
+  none.
+  ## Contributing
+  none.
+  ## Tests
+  none.
+  ## License
+  MIT
+  ## Contact
+  CaitlinCollins
+  caitlin.aileen86@gmail.com
--- index.js
+++ index.js
@@ -57,6 +57,50 @@ inquirer.prompt([
 
     },
     
-  ])
+  ]).then((responsesObj) => {
+
+    let title = responsesObj.title;
+    let description = responsesObj.description;
+    let installation = responsesObj.installation;
+    let usage = responsesObj.usage;
+    let contributing = responsesObj.contributing;
+    let tests = responsesObj.tests;
+    let license = responsesObj.license;
+    let github = responsesObj.github;
+    let email = responsesObj.email;
+  
+    console.log(responsesObj);
+  
+    fileBody = 
+    `# ${title}
+  ## Table of Contents
+  [Description](https://github.com/${github}/${title}#description)
+  [Installation](https://github.com/${github}/${title}#installation)
+  [Usage](https://github.com/${github}/${title}#usage)
+  [Contributing](https://github.com/${github}/${title}#contributing)
+  [Tests](https://github.com/${github}/${title}#tests)
+  [License](https://github.com/${github}/${title}#license)
+  [Contact](https://github.com/${github}/${title}#contact)
+  ## Description
+  ${description}
+  ## Installation
+  ${installation}
+  ## Usage
+  ${usage}
+  ## Contributing
+  ${contributing}
+  ## Tests
+  ${tests}
+  ## License
+  ${license}
+  ## Contact
+  ${github}
+  ${email}`
+
+    fs.writeFile("README.md", fileBody, (err) =>
+      err ? console.log(err) : console.log("File Written!")
+    )
+  
+  });
 
 
